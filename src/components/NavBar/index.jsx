367022b181a6712efe0cec9741a44d5c9f4cfb42
maxisantisi <maxisantisi@gmail.com>
2023-03-02 00:18:37 -0300

    bootstrap react nav

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,22 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import CardWidget from "../CardWidget";
 import "./navbar.css" ;
+
 
 
 function NavBar () {
     return (
         <div className="navbar">
-            <ul>
-                <li>
-                    <button>Adidas</button>
-                </li>
-                <li>
-                    <button>Nike</button>
-                </li>
-                <li>
-                    <button>Tienda</button>
-                </li>
-            </ul>
-            <CardWidget />
+            <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand  href="#home"> <img className="logo" src="https://static-cse.canva.com/blob/954937/1600w-2YtEuuiE8U4.jpg" alt="" /></Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+
+          <Nav className="me-auto">
+
+            <Nav.Link href="#home">Nike</Nav.Link>
+            <Nav.Link href="#home">Adidas</Nav.Link>
+            <Nav.Link href="#home">Puma</Nav.Link>
+            
+          </Nav>
+
+          <CardWidget />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+            
         </div>
     );
 
